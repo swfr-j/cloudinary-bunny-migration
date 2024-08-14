9100cc79c6c016857c1a8ab57e32f7358b159fb4
swfr-j <130271263+swfr-j@users.noreply.github.com>
2024-08-14 11:06:16 +0530

    Simplify BCDN class

--- bunnyNet.js
+++ bunnyNet.js
@@ -1,17 +1,10 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 import { createReadStream } from "fs";
-
-import uniqueNameGenerator from "./uniqueNameGenerator";
-import checkFileType from "./checkFileType";
 
 const httpMode = process.env.BUNNYCDN_HTTP_MODE || "https";
 
-// TODO - Check if this is working with the whole project
 class BCDN {
   headers;
-  imageFolder;
-  otherFolder;
-  videoFolder;
   publicDomain;
   baseUrl;
   axios;
@@ -20,11 +13,7 @@ class BCDN {
     apiKey,
     baseURLDomain,
     storageZone,
-    imageFolder,
-    videoFolder,
-    otherFolder,
     publicDomain,
-    storageZoneRegion = "de"
   ) {
     this.headers = {
       AccessKey: apiKey,
@@ -32,16 +21,8 @@ class BCDN {
       Accept: "application/json",
     };
 
-    this.imageFolder = imageFolder;
-    this.otherFolder = otherFolder;
-    this.videoFolder = videoFolder;
     this.publicDomain = publicDomain;
-
-    if (storageZoneRegion === "de" || storageZoneRegion === "") {
-      this.baseUrl = `${httpMode}://${baseURLDomain}/${storageZone}`;
-    } else {
-      this.baseUrl = `${httpMode}://${storageZoneRegion}.${baseURLDomain}/${storageZone}`;
-    }
+    this.baseUrl = `${httpMode}://${storageZoneRegion}.${baseURLDomain}/${storageZone}`;
 
     this.axios = axios.create({
       baseURL: this.baseUrl,
@@ -50,63 +31,12 @@ class BCDN {
     });
   }
 
-  getPath(fileName) {
-    const fileType = checkFileType(fileName);
-    let fileN = fileName;
-    if (fileName.startsWith("/")) {
-      fileN = fileN.substring(1);
-    }
-    if (fileType === "image") {
-      return `${this.imageFolder}/${fileN}`;
-    }
-    if (fileType === "video") {
-      return `${this.videoFolder}/${fileN}`;
-    }
-    return `${this.otherFolder}/${fileN}`;
-  }
-
   getPublicURL(path) {
     return `${this.publicDomain}${path}`;
   }
 
-  async deleteFile(fileName) {
-    return new Promise((resolve, reject) => {
-      if (fileName === undefined) reject(new Error("No file name provided"));
-
-      const path = this.getPath(fileName);
-      this.axios
-        .delete(path)
-        .then((res) => {
-          if (res.status !== 200)
-            reject(new Error(`Error deleting file: ${res.status}`));
-          resolve(res.data);
-        })
-        .catch((err) => {
-          reject(err.response.data);
-        });
-    });
-  }
-
-  async downloadFile(fileName) {
-    return new Promise((resolve, reject) => {
-      if (fileName === undefined) reject(new Error("No file name provided"));
-
-      const path = this.getPath(fileName);
-      this.axios
-        .get(path)
-        .then((res) => {
-          if (res.status !== 200)
-            reject(new Error(`Error downloading file: ${res.status}`));
-          resolve(res.data);
-        })
-        .catch((err) => {
-          reject(err.response.data);
-        });
-    });
-  }
-
   // fileOrPath: Buffer | string (path)
-  async uploadFile(fileOrPath, fileName) {
+  async uploadFile(fileOrPath, fileName, path) {
     return new Promise((resolve, reject) => {
       let file;
       if (Buffer.isBuffer(fileOrPath)) {
@@ -115,8 +45,9 @@ class BCDN {
         file = createReadStream(fileOrPath);
       }
 
-      const fileN = uniqueNameGenerator(fileName, Date.now());
-      const path = this.getPath(fileN);
+      const fileN = fileName;
+      if (!path.startsWith("/")) path = `/${path}`;
+
       const options = {
         headers: {
           "content-type": "application/octet-stream",
@@ -148,9 +79,6 @@ const bcdn = new BCDN(
   process.env.BUNNYCDN_API_KEY,
   process.env.BUNNYCDN_API_DOMAIN,
   process.env.BUNNYCDN_STORAGE_ZONE,
-  process.env.BUNNYCDN_API_PATH_IMAGES,
-  process.env.BUNNYCDN_API_PATH_VIDEOS,
-  process.env.BUNNYCDN_API_PATH_RAW,
   process.env.BUNNYCDN_PUBLIC_DOMAIN
 );
 
