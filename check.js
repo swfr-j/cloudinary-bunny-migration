0fd1f9fa50d38a9d8550bdaff1f4996544361583
swfr-j <130271263+swfr-j@users.noreply.github.com>
2024-08-15 14:36:07 +0530

    Add check command

--- check.js
+++ check.js
@@ -1,9 +1,21 @@
 import axios from "axios";
-import { csvReader } from "./csvHelpers";
+import { csvReader, csvErrorWriter } from "./csvHelpers";
 import logger from "./logger";
 
-const processBatch = (public_id, cloudinary_url, bunny_url) => {
-    console.log("batch:", public_id, cloudinary_url, bunny_url);
+const processBatch = async (public_id, cloudinary_url, bunny_url) => {
+    if (!bunny_url) { return; }
+    try {
+        const res = await axios.head(bunny_url);
+
+        if (res.status !== 200) {
+            throw new Error(`Url not working: ${bunny_url}`);
+        } else {
+            logger.info(`Url working: ${bunny_url}`);
+        }
+    } catch (error) {
+        logger.error(`Error checking URL: ${bunny_url}`, error);
+        await csvErrorWriter.writeRecords([{ public_id, cloudinary_url, bunny_url, error: error.message }]);
+    }
 }
 
 const readCsvInBatches = async () => {
--- csvHelpers.js
+++ csvHelpers.js
@@ -2,8 +2,12 @@ import path from 'path';
 import { EOL } from 'os';
 import { existsSync, createReadStream } from 'fs';
 import { createObjectCsvWriter } from 'csv-writer';
+import PQueue from "p-queue";
+
+const queue = new PQueue({ concurrency: 10 });
 
 const csvFilePath = path.resolve(__dirname, 'data.csv');
+
 export const csvWriter = createObjectCsvWriter({
     path: csvFilePath,
     header: [
@@ -14,7 +18,18 @@ export const csvWriter = createObjectCsvWriter({
     append: existsSync(csvFilePath), // Append if the file already exists
 });
 
-const parseChunk = (text, index, processDataCallback) => {
+export const csvErrorWriter = createObjectCsvWriter({
+    path: path.resolve(__dirname, 'errors.csv'),
+    header: [
+        { id: 'public_id', title: 'public_id' },
+        { id: 'cloudinary_url', title: 'cloudinary_url' },
+        { id: 'bunny_url', title: 'bunny_url' },
+        { id: 'error', title: 'error' }
+    ],
+    append: existsSync(path.resolve(__dirname, 'errors.csv')), // Append if the file already exists
+});
+
+const parseChunk = async (text, index, processDataCallback) => {
     if (index === 0) {
         // First chunk contains the header
         let headerLine = text.substring(0, text.indexOf(EOL));
@@ -26,8 +41,10 @@ const parseChunk = (text, index, processDataCallback) => {
     for(let line of lines) {
         let values = line.split(',');
         const [public_id, cloudinary_url, bunny_url] = values;
-        processDataCallback(public_id, cloudinary_url, bunny_url);
+        queue.add(async () => processDataCallback(public_id, cloudinary_url, bunny_url));
     }
+
+    await queue.onIdle();
 }
 
 export const csvReader = async (processDataCallback) => {
@@ -35,9 +52,9 @@ export const csvReader = async (processDataCallback) => {
         let chunkCount = 0;
         const readStream = createReadStream(csvFilePath);
 
-        readStream.on('data', chunk => {
+        readStream.on('data', async chunk => {
             const text = chunk.toString();
-            parseChunk(text, chunkCount, processDataCallback);
+            await parseChunk(text, chunkCount, processDataCallback);
             chunkCount++;
         });
 
